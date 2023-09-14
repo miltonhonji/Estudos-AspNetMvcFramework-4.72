83bbbc957547a87017758a161bfdb33334f175c3
Milton Honji <factorysolutionIT@outlook.com>
2023-09-14 17:47:46 -0300

    Adicionando novos itens

--- Projeto01/Projeto01/Controllers/CategoriasController.cs
+++ Projeto01/Projeto01/Controllers/CategoriasController.cs
@@ -2,16 +2,46 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Projeto01.Models;
 using System.Web.Mvc;
 
 namespace Projeto01.Controllers
 {
     public class CategoriasController : Controller
     {
+        private static IList<Categoria> categorias = new List<Categoria>()
+        {
+            new Categoria()
+            {
+                CategoriaId = 1,
+                Nome = "Notebooks"
+            },
+            new Categoria()
+            {
+                CategoriaId = 2,
+                Nome = "Monitores"
+            },
+            new Categoria()
+            {
+                CategoriaId = 3,
+                Nome = "Impressoras"
+            },
+            new Categoria()
+            {
+                CategoriaId = 4,
+                Nome = "Mouses"
+            },
+            new Categoria()
+            {
+                CategoriaId = 5,
+                Nome = "Desktops"
+            }
+        };
+
         // GET: Categorias
         public ActionResult Index()
         {
-            return View();
+            return View(categorias);
         }
     }
 }
