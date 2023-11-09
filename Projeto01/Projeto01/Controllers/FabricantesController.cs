cd2882acf489e9ace6bfb6ffad00d589c8f51f39
milton.honji <factorysolutionIt@outlook.com>
2023-11-09 13:56:11 -0300

    Adicionado alteração do fabricante

---
+++ Projeto01/Projeto01/Controllers/FabricantesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Projeto01.Contexts;   
+
+namespace Projeto01.Controllers
+{
+    public class FabricantesController : Controller
+    {
+        private EFContext context = new EFContext();
+
+        // GET: Fabricantes
+        public ActionResult Index()
+        {
+            return View(context.Fabricantes.OrderBy(
+                c => c.Nome));
+        }
+    }
+}
